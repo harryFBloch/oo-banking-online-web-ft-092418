15cb19169cd29b2173046fa9c872c2c5cc07e537
Harry Bloch <harryfbloch@gmail.com>
2018-10-04 02:09:51 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -31,7 +31,7 @@ class Transfer
   end
   
   def reverse_transfer
-    if self.status == "comlete"
+    if self.status == "complete"
       self.sender.deposit(self.amount)
       self.receiver.balance -= self.amount
       self.status = "reversed"
