e7640e414e0867b91e6378f897e591a291f3d46d
Harry Bloch <harryfbloch@gmail.com>
2018-10-04 01:52:21 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -23,7 +23,7 @@ class Transfer
     self.sender.balance -= amount
     self.status = "complete"
     else
-      
+      self.status = "Transaction rejected. Please check your account balance."
    end
   end
   
