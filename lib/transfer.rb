0e4c32a8031475e02fdbaa43c35cb7c914e04900
Harry Bloch <harryfbloch@gmail.com>
2018-10-04 01:39:25 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,11 +1,12 @@
+require 'pry'
 class Transfer
   attr_accessor :sender, :to, :amount
   
   def initialize(from, to, amount)
-    
     self.to = to
     self.amount = amount
     self.sender = BankAccount.new(from)
+    binding.pry
   end
   
   def sender 
