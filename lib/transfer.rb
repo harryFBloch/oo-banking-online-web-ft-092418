483e9d7de56c9540fbf4f50d2ef4eb21c70073b2
Harry Bloch <harryfbloch@gmail.com>
2018-10-04 01:46:51 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,11 +1,12 @@
 require 'pry'
 class Transfer
-  attr_accessor :sender, :receiver, :amount
+  attr_accessor :sender, :receiver, :amount, :status
   
   def initialize(from, to, amount)
     self.receiver = to
     self.amount = amount
     self.sender = from
+    self.status = "pending"
   end
   
   
